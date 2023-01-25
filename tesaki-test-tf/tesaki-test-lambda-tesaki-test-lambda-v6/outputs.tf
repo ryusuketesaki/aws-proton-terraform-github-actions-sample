ef557df39ccc43888615ee8bf2faeb24c63b73cd
sbx_user1051 <sbx_user1051@169.254.144.129>
2023-01-25 10:08:26 +0000

    Update requested for Proton - service/tesaki-test-lambda/service-instance/tesaki-test-lambda-v6

---
+++ tesaki-test-tf/tesaki-test-lambda-tesaki-test-lambda-v6/outputs.tf
@@ -0,0 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:808225968452:service/tesaki-test-lambda/service-instance/tesaki-test-lambda-v6
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "lambda_arn" {
+  value = "whocares"
+}

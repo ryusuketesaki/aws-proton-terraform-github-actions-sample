23c613fdcf00801addb84dafcf9fe5180d82c5aa
sbx_user1051 <sbx_user1051@169.254.22.149>
2023-01-30 11:47:31 +0000

    Update requested for Proton - service/tesaki-test-230130/service-instance/tesaki-test-srv-230130

---
+++ tesaki-test-env-230130/tesaki-test-230130-tesaki-test-srv-230130/outputs.tf
@@ -0,0 +1,23 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:808225968452:service/tesaki-test-230130/service-instance/tesaki-test-srv-230130
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "ServiceSqsDeadLetterQueueName" {
+  value = aws_sqs_queue.ecs_processing_dlq.name
+}
+
+output "ServiceSqsDeadLetterQueueArn" {
+  value = aws_sqs_queue.ecs_processing_dlq.arn
+}
+
+output "ServiceSqsQueueName" {
+  value = aws_sqs_queue.ecs_processing_queue.name
+}
+
+output "ServiceSqsQueueArn" {
+  value = aws_sqs_queue.ecs_processing_queue.arn
+}

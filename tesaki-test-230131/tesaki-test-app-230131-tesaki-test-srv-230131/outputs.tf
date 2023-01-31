f8ba3159fe626de53834b7df0edc4b214e63a8d1
ryusuketesaki <114964960+ryusuketesaki@users.noreply.github.com>
2023-01-31 16:31:06 +0900

    Delete tesaki-test-230131/tesaki-test-app-230131-tesaki-test-srv-230131 directory

--- tesaki-test-230131/tesaki-test-app-230131-tesaki-test-srv-230131/outputs.tf
+++
@@ -1,7 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-output "ServiceUrl" {
-  value = "https://${aws_lb.service_lb.dns_name}"
-}

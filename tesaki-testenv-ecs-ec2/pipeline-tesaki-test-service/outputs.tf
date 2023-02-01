cfd9701b3ae86369b7741f0956a3df85ebab6b47
sbx_user1051 <sbx_user1051@169.254.45.45>
2023-02-01 06:15:55 +0000

    Update requested for Proton - service/pipeline/service-instance/tesaki-test-service

---
+++ tesaki-testenv-ecs-ec2/pipeline-tesaki-test-service/outputs.tf
@@ -0,0 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:808225968452:service/pipeline/service-instance/tesaki-test-service
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "ServiceUrl" {
+  value = "https://${aws_lb.service_lb.dns_name}"
+}

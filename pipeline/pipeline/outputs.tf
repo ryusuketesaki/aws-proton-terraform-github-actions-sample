f2bf816743e2bfb3046a43868d3701389667fb74
ryusuketesaki <114964960+ryusuketesaki@users.noreply.github.com>
2023-02-01 11:35:59 +0900

    Delete pipeline/pipeline directory

--- pipeline/pipeline/outputs.tf
+++
@@ -1,7 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-output "PipelineEndpoint" {
-  value = "https://${local.region}.console.aws.amazon.com/codesuite/codepipeline/pipelines/${aws_codepipeline.pipeline.id}/view?region=${local.region}"
-}

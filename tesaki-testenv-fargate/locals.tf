8d58c9fdf2c8694ade228569783cea5b14e7bf36
ryusuketesaki <114964960+ryusuketesaki@users.noreply.github.com>
2023-02-09 11:46:08 +0900

    Delete tesaki-testenv-fargate directory

--- tesaki-testenv-fargate/locals.tf
+++
@@ -1,9 +0,0 @@
-/*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
-*/
-
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.id
-  partition  = data.aws_partition.current.id
-}
